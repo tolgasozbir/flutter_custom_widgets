bb3abb4b26ee6e6f5360f57bebc8984e59e1147d
tolgasozbir <tolgasoz1@gmail.com>
2022-07-12 01:31:59 +0300

    battle card last changes

--- lib/battle_card/battle_screen_view.dart
+++ lib/battle_card/battle_screen_view.dart
@@ -11,34 +11,47 @@ class BattleScreenView extends StatefulWidget {
 }
 
 class _BattleScreenViewState extends State<BattleScreenView> {
+
+  int index = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: _bodyView(),
+      body: SafeArea(child: _bodyView()),
     );
   }
 
   Widget _bodyView() {
-    return Column(
-      children: [   
-        Expanded(
-          child: BattleCard(
-            cardPosition: CardPosition.Top,
-            child: Image.network("https://picsum.photos/400/400?random=1",fit: BoxFit.cover,),
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        image: DecorationImage(image: NetworkImage("https://designshack.net/wp-content/uploads/best-subtle-black-white-background-textures.jpg"),fit: BoxFit.cover)
+      ),
+      child: Column(
+        children: [   
+          Expanded(
+            child: BattleCard(
+              cardPosition: CardPosition.Top,
+              child: Image.network("https://picsum.photos/400/400?random=${index % 2 == 0 ? index : index+2}",fit: BoxFit.cover,),
+            ),
           ),
-        ),
-        spacerDivider(),
-        Expanded(
-          child: BattleCard(
-          cardPosition: CardPosition.Bottom,
-            child: Image.network("https://picsum.photos/400/400?random=2",fit: BoxFit.cover,),
-            cardResults: (bool? didBottomCardWin){
-              print("didBottomCardWin $didBottomCardWin");
-            },
+          spacerDivider(),
+          Expanded(
+            child: BattleCard(
+            cardPosition: CardPosition.Bottom,
+              child: Image.network("https://picsum.photos/400/400?random=${index % 2 == 1 ? index : index+2}",fit: BoxFit.cover,),
+              cardResults: (bool? didBottomCardWin) async {
+                print("didBottomCardWin $didBottomCardWin");
+                if (didBottomCardWin != null) {
+                  await Future.delayed(Duration(milliseconds: 400));
+                  setState(() {
+                    index++;
+                  });
+                }
+              },
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 
